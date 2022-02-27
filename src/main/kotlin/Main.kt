e31b23a7caebdca7cf564dbebcb1b65b3873094c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-02-27 12:33:01 +0900

    Adapt theme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,29 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
-
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    Window(onCloseRequest = ::exitApplication, title = "ImageDiff") {
+        MainTheme(isDarkMode = false) {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                var text by remember { mutableStateOf("Hello, World!") }
+
+                Button(onClick = {
+                    text = "Hello, Desktop!"
+                }, modifier = Modifier.wrapContentHeight()) {
+                    Text(text)
+                }
+            }
+        }
     }
 }
---
+++ src/main/kotlin/resource/Strings.kt
@@ -0,0 +1,5 @@
+package resource
+
+object Strings {
+    const val APP_VERSION = "1.0"
+}
