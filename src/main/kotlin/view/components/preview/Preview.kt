e1cdfc9e0a4a7c80955e53e088e4a7cea761e27f
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-05-05 14:43:59 +0900

    Fix preview

--- src/main/kotlin/view/components/preview/Preview.kt
+++ src/main/kotlin/view/components/preview/Preview.kt
@@ -1,16 +1,35 @@
 package view.components.preview
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Preview(bitmap: ImageBitmap?, modifier: Modifier = Modifier) {
-    Box(modifier) {
+    Row(modifier) {
+        PreviewImage(bitmap, modifier = Modifier.weight(weight = 0.5f, fill = true).fillMaxSize())
+
+        Spacer(modifier = Modifier.width(1.dp).fillMaxHeight().background(Color.LightGray))
+
+        PreviewImage(bitmap, modifier = Modifier.weight(weight = 0.5f, fill = true).fillMaxSize())
+    }
+}
+
+@Composable
+private fun PreviewImage(bitmap: ImageBitmap?, modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
         if (bitmap != null) {
             Image(bitmap = bitmap, contentDescription = "", modifier = Modifier.align(Alignment.Center))
         } else {
